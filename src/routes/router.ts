eb05eebeac20d3ac313784c0f9106d3ba135b6ed
0xrusowsky <0xrusowsky@proton.me>
2023-01-23 12:39:36 +0100

    Improve router setup

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,8 +2,7 @@
 
 import { TwitterApi } from "twitter-api-v2"
 import { getBookmarks, processBookmarks } from "../utils"
-import { Application } from "express"
-import session from "express-session"
+import { Router, Application } from "express"
 import dotenv from "dotenv"
 
 dotenv.config()
@@ -13,62 +12,57 @@ const CALLBACK_URL = process.env.CALLBACK_URL
 const vaultPath = process.env.VAULT_PATH
 const filePath = vaultPath + process.env.UNPROCESSED_NAME
 
-export const register = (app: Application) => {
-    app.use(session({
-        secret: 'a lil project to synch my bookmarks',
-        resave: false,
-        saveUninitialized: false
-    }))
+const router = Router()
+export default router
 
-    // Define a route handler for the default home page
-    app.get("/", (request, response) => {
-        response.render("index")
-    })
+// Define a route handler for the default home page
+router.get("/", (request, response) => {
+    response.render("index")
+})
 
-    app.get("/test", (request, response) => {
-        response.render("index")
-    })
+router.get("/test", (request, response) => {
+    response.render("index")
+})
 
-    app.get('/auth', (request, response) => {
-        // Obtain access token
-        const client = new TwitterApi({ clientId: CLIENT_ID, clientSecret: CLIENT_SECRET })
+router.get('/auth', (request, response) => {
+    // Obtain access token
+    const client = new TwitterApi({ clientId: CLIENT_ID, clientSecret: CLIENT_SECRET })
 
-        const { url, codeVerifier, state, codeChallenge } = client.generateOAuth2AuthLink(CALLBACK_URL, { scope: ['tweet.read', 'users.read', `bookmark.read`] })
+    const { url, codeVerifier, state, codeChallenge } = client.generateOAuth2AuthLink(CALLBACK_URL, { scope: ['tweet.read', 'users.read', `bookmark.read`] })
 
-        request.session.user = {
-            state,
-            codeVerifier,
-            codeChallenge
-        }
+    request.session.user = {
+        state,
+        codeVerifier,
+        codeChallenge
+    }
 
-        return response.redirect(url)
-    })
+    return response.redirect(url)
+})
 
-    app.get('/oauth/callback', (request, response) => {
-        const { codeVerifier, state: sessionState } = request.session.user
+router.get('/oauth/callback', (request, response) => {
+    const { codeVerifier, state: sessionState } = request.session.user
 
-        const state = request.query.state
-        const code = String(request.query.code)
+    const state = request.query.state
+    const code = String(request.query.code)
 
-        if (!codeVerifier || !state || !sessionState || !code) {
-            console.error('You denied the app or your session expired!')
-            return response.render("error")
-        }
-        if (state !== sessionState) {
-            console.error('Stored tokens didnt match!')
-            return response.render("error")
-        }
+    if (!codeVerifier || !state || !sessionState || !code) {
+        console.error('You denied the app or your session expired!')
+        return response.render("error")
+    }
+    if (state !== sessionState) {
+        console.error('Stored tokens didnt match!')
+        return response.render("error")
+    }
 
-        // Obtain access token
-        const client = new TwitterApi({ clientId: CLIENT_ID, clientSecret: CLIENT_SECRET })
+    // Obtain access token
+    const client = new TwitterApi({ clientId: CLIENT_ID, clientSecret: CLIENT_SECRET })
 
-        client.loginWithOAuth2({ code, codeVerifier, redirectUri: CALLBACK_URL })
-            .then(async (authData) => {
-                const bookmarks = await getBookmarks(authData.client)
-                const output = processBookmarks(bookmarks, vaultPath, filePath)
+    client.loginWithOAuth2({ code, codeVerifier, redirectUri: CALLBACK_URL })
+        .then(async (authData) => {
+            const bookmarks = await getBookmarks(authData.client)
+            const output = processBookmarks(bookmarks, vaultPath, filePath)
 
-                return output ? response.render("success") : response.render("error")
-            })
-            .catch(() => response.status(403).send('Invalid verifier or access tokens!'))
-    })
-}
+            return output ? response.render("success") : response.render("error")
+        })
+        .catch(() => response.status(403).send('Invalid verifier or access tokens!'))
+})
